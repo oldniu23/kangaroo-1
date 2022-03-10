7bd8d53a900aff7e5e717dd12efe2785d2408b43
oldniu <1241677287@qq.com>
2022-03-10 23:11:14 +0800

    创建Layout组件和slot插槽

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,30 +1,17 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">
-      <p>labels.vue</p>
-    </div>
-    <hr />
-    <Nav />
-  </div>
+  <Layout>
+    <p>labels.vue</p>
+  </Layout>
 </template>
 
 <script lang="ts">
+import Layout from "@/components/Layout.vue";
 import Vue from "vue";
 
-export default Vue.extend({});
+export default Vue.extend({
+  components: { Layout },
+});
 </script>
 
 <style lang="scss" scoped>
-.nav-wrapper {
-  border: 1px solid green;
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-}
-.content {
-  //内容多了可以滚动
-  overflow: auto;
-  //尽量把内容给content
-  flex-grow: 1;
-}
 </style>
