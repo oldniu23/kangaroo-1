6c78c9ae748374016bf4e6fe2e0b226578b72da0
oldniu <1241677287@qq.com>
2022-03-14 18:39:35 +0800

    完成types组件的切换功能

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,39 +1,26 @@
 <template>
   <div>
     <ul class="types">
-      <li :class="type === '-' && 'selected'" @click="selectTypes('-')">
-        支出
-      </li>
-      <li :class="type === '+' && 'selected'" @click="selectTypes('+')">
-        收入
-      </li>
+      <li :class="type === '-' && 'selected'" @click="selectType('-')">支出</li>
+      <li :class="type === '+' && 'selected'" @click="selectType('+')">收入</li>
     </ul>
   </div>
 </template>
 
-<script lang="js">
+<script lang="ts">
 import Vue from "vue";
+import Component from "vue-class-component";
 
-export default Vue.extend({
-  data(){
-    return {
-      type:'-' //-号表示支出 +号表示收入
+@Component
+export default class Types extends Vue {
+  type = "-";
+  selectType(type: string) {
+    if (type !== "+" && type !== "-") {
+      throw new Error("未知类型");
     }
-  },
-  props:['xxx'],
-  mounted(){
-    console.log(this.xxx);
-  },
-  methods:{
-    selectTypes(type){
-      //类型只能是+- 否则抛出错误
-      if(type !=='+' && type !=='-'){
-        throw new Error('未知类型');
-      }
-      this.type = type
-    }
+    this.type = type;
   }
-});
+}
 </script>
 
 <style lang="scss" scoped>
