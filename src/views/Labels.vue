61ea22f7a78f894a75a3dd72f3c2c7b4c6f5d2cb
oldniu <1241677287@qq.com>
2022-03-17 22:19:04 +0800

    点击标签可跳转至编辑页

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,11 +1,16 @@
 <template>
   <Layout>
-    <ol class="tags">
-      <li v-for="tag in tags" :key="tag.id">
+    <div class="tags">
+      <router-link
+        class="tag"
+        v-for="tag in tags"
+        :key="tag.id"
+        :to="`/labels/edit/${tag.id}`"
+      >
         <span>{{ tag.name }}</span>
         <Icon name="right" />
-      </li>
-    </ol>
+      </router-link>
+    </div>
     <div class="createTag-wrapper">
       <button class="createTag" @click="createTag">新建标签</button>
     </div>
@@ -46,7 +51,7 @@ export default class Labels extends Vue {
   padding-left: 16px;
   padding-right: 16px;
 
-  > li {
+  > .tag {
     min-height: 44px;
     display: flex;
     align-items: center;
