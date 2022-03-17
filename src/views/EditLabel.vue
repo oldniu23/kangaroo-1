e1b2f897fcad38cf9d68ea00b0345ff41c2ea3de
oldniu <1241677287@qq.com>
2022-03-17 22:08:15 +0800

    新增EditLabel组件

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -8,9 +8,24 @@
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
 import Layout from "@/components/Layout.vue";
+import tagListModel from "@/models/tagListModel";
 
 @Component({ components: { Layout } })
-export default class EditLabel extends Vue {}
+export default class EditLabel extends Vue {
+  created() {
+    const id = this.$route.params.id;
+    tagListModel.fetch();
+    const tags = tagListModel.data;
+    //filter返回的是数组
+    const tag = tags.filter((t) => t.id === id)[0];
+    if (tag) {
+      console.log(tag);
+    } else {
+      //标签不存在就重定向到404页面
+      this.$router.replace("/404");
+    }
+  }
+}
 </script>
 
 <style scoped>
